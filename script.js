56c6f0e3a0e9c80ef284a55d76e756918acac1b2
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-05 19:56:29 +0100

    Added boxes in the grid

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let grid = document.getElementById('grid')
 for(let row in Array.from(Array(8))){
     for(let column in Array.from(Array(8))){
         console.log(row,column)
+        let box = document.createElement('div')
+        box.className = "case"
+        grid.appendChild(box)
     }
 }
