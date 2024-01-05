9e3fff3c8b87a69fe4ad59812703c2118e60f8b7
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-05 20:12:30 +0100

    Colored the board

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ for(let row in Array.from(Array(8))){
     for(let column in Array.from(Array(8))){
         console.log(row,column)
         let box = document.createElement('div')
-        box.className = "case"
-        box.innerText = "e"
+        box.className = "case color"+(((row - column) %2) ? 2 : 1);
         grid.appendChild(box)
     }
 }
