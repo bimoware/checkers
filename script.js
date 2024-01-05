f0b288a4ef1887eb8898122dd6778a117b4e54dd
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-05 20:08:04 +0100

    Created square grids

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ for(let row in Array.from(Array(8))){
         console.log(row,column)
         let box = document.createElement('div')
         box.className = "case"
+        box.innerText = "e"
         grid.appendChild(box)
     }
 }
