07c00e5aa7faffb3d346a733fdd3d3608c50ebb3
bimoware <ptdrrrrr.t.ki@gmail.com>
2024-01-05 20:13:05 +0100

    Added IDs to each box

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ for(let row in Array.from(Array(8))){
     for(let column in Array.from(Array(8))){
         console.log(row,column)
         let box = document.createElement('div')
+        box.id = `${row}${column}`
         box.className = "case color"+(((row - column) %2) ? 2 : 1);
         grid.appendChild(box)
     }
